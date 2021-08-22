4d681def5e4fcf2fe7b3c2fec4e9432f87edc057
Marvin <trujillomarvin@hotmail.com>
2021-08-21 18:42:33 -0700

    solved grid issue

--- dom.js
+++ dom.js
@@ -1,1 +1,18 @@
 console.log("Hello World!")
+
+const grid = document.getElementById("grid");
+//const cell = document.createElement('div');
+
+/* cell.innerHTML = "test";
+grid.appendChild(cell); */
+
+function makeGrid() {
+    for (let i = 0; i < 255; i++) {
+        const cell = document.createElement('div');
+        cell.innerHTML = "3";
+        grid.appendChild(cell).className = 'cell';
+        
+    }
+}
+
+makeGrid();
