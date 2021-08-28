314e89c77002f329ffcf2f3e33a6e291d46d56ad
Marvin <trujillomarvin@hotmail.com>
2021-08-27 21:02:43 -0700

    im going crazy

--- dom.js
+++ dom.js
@@ -1,18 +1,34 @@
 console.log("Hello World!")
 
 const grid = document.getElementById("grid");
-//const cell = document.createElement('div');
+
+
+//console.log(cell);
 
 /* cell.innerHTML = "test";
 grid.appendChild(cell); */
+
+
+
 
 function makeGrid() {
     for (let i = 0; i < 255; i++) {
         const cell = document.createElement('div');
         cell.innerHTML = "3";
         grid.appendChild(cell).className = 'cell';
-        
+
     }
 }
 
+
+
+const cell = document.getElementsByClassName('cell');
+console.log(cell); 
+
+function cellHov() {
+    for(var i = 0; i < cell.length; i ++) {
+        cell[i].style.className='cellHov';
+    }
+   // document.addEventListener('mouseover', 
+}
 makeGrid();
