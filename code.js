30555b6091505b730ccd8f6af963db0a8b955823
Marvin <trujillomarvin@hotmail.com>
2021-10-25 13:42:32 -0700

    messing w/ js

---
+++ code.js
@@ -0,0 +1,31 @@
+let log = console.log;
+
+// const nine = document.querySelector("#nine");
+// const eight = document.querySelector("#eight");
+// let x = addEventListener('onclick');
+
+const clear = document.querySelector("#clear")
+log(clear);
+
+
+
+
+
+const add = function(x, y) {
+    return x + y;
+}
+
+const subtract = function(x, y) {
+    return x - y;
+}
+
+const divide = function(x, y) {
+    return x / y;
+}
+
+const multiply = function(x, y) {
+    return x * y;
+}
+
+
+log(add(3,4));
