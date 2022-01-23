ddb14fb5d371713af305b8ebb502b259280cc4ef
marvo <trujillomarvin@hotmail.com>
2022-01-23 11:51:34 -0800

    need to establish storage params

--- code.js
+++ code.js
@@ -2,49 +2,65 @@ let log = console.log;
 log("hello world")
 
 
-
+let store = "";
 
 const equalsButton = document.querySelector("#equal")
 const allClearButton = document.querySelector("#clear")
+const operatorButtons = document.querySelectorAll(".operators")
+const numberButtons = document.querySelectorAll(".btn")
+const screenText = document.querySelector('#result');
+const keyPad = document.querySelector('#numbers');
 
-const operatorButtons = Array.from(document.getElementsByClassName("operators"))
-const numberButtons = Array.from(document.getElementsByClassName("btn"))
+
+// let operatorText = keyPad.addEventListener('click', operatorInput);
+
+let calculation = equalsButton.addEventListener('click', calc);
 
 
-operatorButtons.map(function (button) {
-    button.addEventListener("click", function (e) {
-        console.log(e);
-        console.log(e.target);
-        log(e.target.innerText);
-    })
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', operatorInput);
 })
-numberButtons.map(button => {
-    button.addEventListener("click", (e) => {
-    console.log(e);
-    console.log(e.target);
-    console.log(e.target.innerText);
-})})
 
-let firstOperandText = 
+numberButtons.forEach((button) => {
+    button.addEventListener('click', numberInput);
+})
 
+function calc () {
+    parseInt(store);
+    return store;
 
-
-
-const add = function(x, y) {
-    return x + y;
+    // log(store);
 }
 
-const subtract = function(x, y) {
-    return x - y;
+function operatorInput(e) {
+    screenText.innerText = e.target.innerText;
+      calculation = e.target.innerText;
+      store += e.target.innerText;
 }
 
-const divide = function(x, y) {
-    return x / y;
+function numberInput(e) {
+      screenText.innerText = e.target.innerText;
+        calculation = e.target.innerText;
+        store += e.target.innerText;
 }
 
-const multiply = function(x, y) {
-    return x * y;
-}
+
+
+// const add = function(x, y) {
+//     return x + y;
+// }
+
+// const subtract = function(x, y) {
+//     return x - y;
+// }
+
+// const divide = function(x, y) {
+//     return x / y;
+// }
+
+// const multiply = function(x, y) {
+//     return x * y;
+// }
 
 // let btn = document.querySelectorAll(".btn").forEach(function(e) {
 //     e.addEventListener("click", function() {
