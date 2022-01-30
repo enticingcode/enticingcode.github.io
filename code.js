39ef8a4ca932e7d91ee9110aa0ddd05f5f7c8236
Marvin <trujillomarvin@hotmail.com>
2022-01-29 19:13:00 -0800

    shit fuck

--- code.js
+++ code.js
@@ -1,9 +1,9 @@
 let log = console.log;
 log("hello world")
 
-let totalResult;
+let totalResult = 0;
 let firstOperand = "";
-let secondOperand = ""
+let secondOperand = "";
 let firstOperator = null;
 let secondOperator = null;
 
@@ -11,90 +11,112 @@ const equalsButton = document.querySelector("#equal")
 const allClearButton = document.querySelector("#clear")
 const operatorButtons = document.querySelectorAll(".operators")
 const numberButtons = document.querySelectorAll(".btn")
-const screenText = document.querySelector('#result');
+const screenNum = document.querySelector('#result');
 const keyPad = document.querySelector('#numbers');
 const calcPreview = document.querySelector("#calcPreview")
 
 
 
-function numbersBtns() {
-  numberButtons.forEach((button) => {
-    button.addEventListener('click', function (e) {
-      if (firstOperator != null && secondOperator === null) {
-
-        secondInput(e);
-      }
-      else if (firstOperator === null) {
-        firstInput(e);
-      }
-      else secondInput(e);
-    }
-    );
-  })
-}
-
-function firstInput(e) {
-  firstOperand += e.target.innerText;
-  screenText.innerText = firstOperand;
-}
-
-function secondInput(e) {
-  secondOperand += e.target.innerText;
-  screenText.innerText = secondOperand;
-  calcPreview.innerText = `${firstOperand} ${firstOperator} ${secondOperand}`;
-
-}
-function operatorInput(e) {
-  firstOperator = e.target.innerText;
-  screenText.innerText = firstOperator;
-}
-
-operatorButtons.forEach((button) => {
-  button.addEventListener('click', operatorInput);
+numberButtons.forEach((button) => {
+    button.addEventListener('click', numberInput)
 })
 
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', operatorInput);
+})
+function operatorInput(e) {
+    if (firstOperator === null) {
+        firstOperator = e.target.value;
+        log(firstOperator);
+        log(`firstOp = ${firstOperator}, secondOp = ${secondOperator}`)
+
+    }
+    else if (firstOperator != null && secondOperator === null) {
+        secondOperator = e.target.value;
+        totalResult = calculate(Number(firstOperand), Number(secondOperand), firstOperator);
+        firstOperand = totalResult;
+        secondOperand = "";
+        log(`firstOp = ${firstOperator}, secondOp = ${secondOperator}`)
+
+    }
+    else if (firstOperator != null && secondOperator != null) {
+        secondOperator = e.target.value;
+        totalResult = calculate(Number(firstOperand), Number(secondOperand), secondOperator);
+        firstOperand = totalResult;
+        secondOperand = "";
+        log(`firstOp = ${firstOperator}, secondOp = ${secondOperator}`)
+
+    }
+    // firstOperator = e.target.innerText;
+    // calcPreview.innerText += firstOperator;
+}
+
+
+function numberInput(e) {
+    if (firstOperand === "") {
+        firstOperand += e.target.value;
+        screenNum.innerText = firstOperand;
+        log(`firstNum = ${firstOperand}, secondNum = ${secondOperand}`);
+
+    }
+    else if (firstOperand != "" && secondOperand == "") {
+        secondOperand += e.target.value;
+        screenNum.innerText = secondOperand;
+        log(`firstNum = ${firstOperand}, secondNum = ${secondOperand}`);
+
+    }
+    else if (firstOperand != "" && secondOperand != "") {
+        log('hi');
+    }
+
+
+}
 equalsButton.addEventListener('click', calculate)
 allClearButton.addEventListener('click', clearAll);
 
-
-
 function clearAll() {
-  firstOperand = "";
-  secondOperand = "";
-  firstOperator = null;
-  secondOperator = null;
-  screenText.innerText = "0";
-  calcPreview.innerText = "";
+    firstOperand = "";
+    secondOperand = "";
+    firstOperator = null;
+    secondOperator = null;
+    screenNum.innerText = 0;
+    calcPreview.innerText = "";
+    totalResult = 0;
+
 }
 
 function calculate() {
-  let x = Number(firstOperand);
-  let y = Number(secondOperand);
-  let op = firstOperator;
-  log(`${x} ${op} ${y}`);
+    x = Number(firstOperand);
+    y = Number(secondOperand);
+    op = firstOperator;
+    op2 = secondOperator;
 
-  if (op === "*") {
-    let result = x * y;
-    result += totalResult;
-  }
-  else if (op === "+") {
-    let result = x + y;
-    result += totalResult;
-  }
-  else if (op === "-") {
-    let result = x - y;
-    result += totalResult;
-  }
-  else if (op === "/") {
-    let result = x / y;
-    result += totalResult;
-  }
-  return totalResult;
+
+    let result;
+
+    if (op != null && op2 === null) {
+        switch (op) {
+            case "*":
+                result = x * y;
+                totalResult = result;
+                break;
+        }
+    }
+    else if (op != null && op2 != null) {
+        switch (op2) {
+            case "*":
+                result = x * y;
+                log(x * y);
+                totalResult = result;
+                break;
+        }
+    }
+    calcPreview.innerText = `${x} ${op} ${y}`;
+    log(x, op, y, totalResult)
+    screenNum.innerText = totalResult;
+    return totalResult;
 }
 
 
-
-
-numbersBtns();
 
 
