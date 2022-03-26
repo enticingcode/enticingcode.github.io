9247bab74a3cf5e37ffbb97003bfe81b2aa8bdb3
Marvin <trujillomarvin@hotmail.com>
2022-03-25 19:53:56 -0500

    initiated project

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+console.log,document.querySelector("#button").addEventListener("click",(function(){alert("sup")}));
---
+++ src/index.js
@@ -0,0 +1,16 @@
+let log = console.log;
+
+
+const button = document.querySelector("#button");
+
+button.addEventListener("click", popUP);
+
+function popUP() {
+    alert("sup")
+}
+
+
+
+
+// alert("Hello marvin");
+// alert("nah")
