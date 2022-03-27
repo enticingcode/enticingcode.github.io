d7f3b1eb1e3aa9d24c2807e3b42b06c936ddf005
Marvin <trujillomarvin@hotmail.com>
2022-03-27 10:41:59 -0500

    working out css positioning for banner/header

--- src/Modules/initial-load.js
+++ src/Modules/initial-load.js
@@ -1,6 +1,31 @@
+let log = console.log;
+
+
 function createHeader(id, content) {
     const header = document.createElement("header");
-    log(header);
+    header.setAttribute("id", id);
+    let logoText = document.createElement("h1");
+    logoText.textContent = content;
+    header.append(logoText);
+    return header
+}
+
+function loadBanner() {
+    // banner container //
+    let banner = document.createElement("div");
+    banner.setAttribute('id', "videoContainer");
+
+    // banner video // 
+    let video = document.createElement("video");
+    video.src = "/video/POKEVID.mp4"
+    video.autoplay = true;
+    video.muted = true;
+    video.loop = true;
+    video.setAttribute('id', 'videoBanner');
+
+    // append video to container //
+    banner.append(video);
+    return banner;
 }
 
 
@@ -12,10 +37,12 @@ function createHeader(id, content) {
 
 
 
-
-
 function loadPage() {
-    console.log("sup");
+    let content = document.querySelector("#content");
+    let header = createHeader("header", "Bienvenidos a NavaMar");
+    let videoContainer = loadBanner();
+    content.append(videoContainer);
+    content.append(header);
 }
 
 
